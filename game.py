ca97a063b718c298d93ea95b7b0ed2fee124f08c
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2023-12-30 11:21:01 +0100

    shapes and user input

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 from settings import *
+from random import choice
+
+from timer import Timer
 
 
 class Game:
@@ -6,6 +9,106 @@ class Game:
         # General
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
+        self.rect = self.surface.get_rect(topleft=(PADDING, PADDING))
+        self.sprites = pygame.sprite.Group()
+
+        # Lines
+        self.line_surface = self.surface.copy()
+        self.line_surface.fill((0, 255, 0))
+        self.line_surface.set_colorkey((0, 255, 0))
+        self.line_surface.set_alpha(120)
+
+        # Tetromino
+        self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
+
+        # Timer
+        self.timers = {
+            "vertical move": Timer(UPDATE_START_SPEED, True, self.move_down),
+            "horizontal move": Timer(MOVE_WAIT_TIME),
+        }
+        for timer in self.timers.values():
+            timer.activate()
+
+    def timer_update(self):
+        for timer in self.timers.values():
+            timer.update()
+
+    def move_down(self):
+        self.tetromino.move_down()
+
+    def draw_grid(self):
+        for col in range(1, COLUMNS):
+            x = col * CELL_SIZE
+            pygame.draw.line(
+                self.line_surface,
+                LINE_COLOR,
+                (x, 0),
+                (x, self.surface.get_height()),
+                1,
+            )
+
+        for row in range(1, ROWS):
+            y = row * CELL_SIZE
+            pygame.draw.line(
+                self.line_surface,
+                LINE_COLOR,
+                (0, y),
+                (self.surface.get_width(), y),
+                1,
+            )
+
+        self.surface.blit(self.line_surface, (0, 0))
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers["horizontal move"].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers["horizontal move"].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers["horizontal move"].activate()
 
     def run(self):
+        self.input()
+        self.timer_update()
+        self.sprites.update()
+
+        # Drawing
+        self.surface.fill(GRAY)
+        self.sprites.draw(self.surface)
+
+        self.draw_grid()
         self.display_surface.blit(self.surface, (PADDING, PADDING))
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+
+class Tetromino:
+    def __init__(self, shape, group):
+        self.block_positions = TETROMINOS[shape]["shape"]
+        self.color = TETROMINOS[shape]["color"]
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
+
+    def move_down(self):
+        for block in self.blocks:
+            block.pos.y += 1
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        # General
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+
+        # Position
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        self.rect = self.image.get_rect(topleft=self.pos * CELL_SIZE)
+
+    def update(self):
+        self.rect.topleft = self.pos * CELL_SIZE
---
+++ timer.py
@@ -0,0 +1,26 @@
+from pygame.time import get_ticks
+
+
+class Timer:
+    def __init__(self, duration, repeated=False, func=None):
+        self.repeated = repeated
+        self.func = func
+        self.duration = duration
+        self.deactivate()
+
+    def activate(self):
+        self.active = True
+        self.start_time = get_ticks()
+
+    def deactivate(self):
+        self.start_time = 0
+        self.active = False
+
+    def update(self):
+        current_time = get_ticks()
+        if current_time - self.start_time >= self.duration and self.active:
+            if self.func and self.start_time != 0:
+                self.func()
+            self.deactivate()
+            if self.repeated:
+                self.activate()
